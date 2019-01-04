c1788dcb40874676a5d139691b16805c47cf820e
Leonardo Rafael Wehrmeister <leonardo@inventti.com.br>
2019-01-04 12:38:29 -0200

    LW: Adicionando chips

--- docs/myrp.182bdb6a.js
+++ docs/myrp.182bdb6a.js
@@ -144,7 +144,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":18}],21:[function(require,module,exports) {
+},{"_css_loader":18}],31:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -313,5 +313,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[21])
+},{}]},{},[31])
 //# sourceMappingURL=/myrp.182bdb6a.map
