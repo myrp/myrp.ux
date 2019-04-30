4588e8f21097ae413f5569e2af95720b2cd07975
Leonardo Rafael Wehrmeister <leonardo@inventti.com.br>
2019-04-30 13:42:37 -0300

    LW: Diminuindo margem da timeline

--- docs/myrp.182bdb6a.js
+++ docs/myrp.182bdb6a.js
@@ -144,7 +144,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":19}],22:[function(require,module,exports) {
+},{"_css_loader":19}],31:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -313,5 +313,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[22])
+},{}]},{},[31])
 //# sourceMappingURL=/myrp.182bdb6a.map
